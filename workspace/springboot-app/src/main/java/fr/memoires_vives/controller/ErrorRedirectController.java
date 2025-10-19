442fb2c271d266059f544899f9417f4a4e0a8293
OpymS <maisonneuve.menuisier@gmail.com>
2025-10-19 21:39:36 +0200

    feat(error): gère l'erreur HTTP 413 (Payload Too Large) Ajoute une gestion spécifique pour l'erreur 413 afin d'informer l'utilisateur lorsque la taille des données envoyées est trop grande. Modification de default.conf car erreur levée par nginx.

---
+++ workspace/springboot-app/src/main/java/fr/memoires_vives/controller/ErrorRedirectController.java
@@ -0,0 +1,27 @@
+package fr.memoires_vives.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class ErrorRedirectController {
+
+	@GetMapping("/upload-error")
+	public String handleUploadError(HttpServletRequest request, Model model) {
+		
+		String referer = request.getHeader("Referer");
+		if (referer == null || referer.isBlank()) {
+			referer = "/";
+		}
+		
+		model.addAttribute("backUrl", referer);
+		
+		return "/error/413";
+	}
+	
+}
