ac8ef5363c33c5389f6af49df39c597171658a9e
OpymS <maisonneuve.menuisier@gmail.com>
2025-10-19 22:01:59 +0200

    fix(security): autorise l'accès à la gestion de l'erreur 413 pour tous les utilisateurs Ajoute `permitAll()` pour permettre à tous les utilisateurs d'accéder à la page de gestion de l'erreur 413.

--- workspace/springboot-app/src/main/java/fr/memoires_vives/controller/ErrorRedirectController.java
+++ workspace/springboot-app/src/main/java/fr/memoires_vives/controller/ErrorRedirectController.java
@@ -3,7 +3,6 @@ package fr.memoires_vives.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
