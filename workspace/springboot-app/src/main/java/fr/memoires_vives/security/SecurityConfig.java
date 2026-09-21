8e14333215ad2c5a257b5a3d122b9bdd022b5498
OpymS <maisonneuve.menuisier@gmail.com>
2026-09-21 23:19:56 +0200

    feat(design): redessine les fiches et formulaires principaux - Redessine la **fiche Memory** (mise en page et styles). - Redessine la **fiche Profil** (affichage et cohérence visuelle). - Met à jour les formulaires :   - **Login** (nouveau design).   - **Signup** (nouveau design).   - **Modification de profil** (styles).   - **Création/Modification de souvenir** (intégration visuelle). - Améliore les **fragments d'erreur** (affichage).

--- workspace/springboot-app/src/main/java/fr/memoires_vives/security/SecurityConfig.java
+++ workspace/springboot-app/src/main/java/fr/memoires_vives/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 		http.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/memory/new").authenticated()
 				.requestMatchers("/", "/home", "/css/**", "/js/**", "/images/public/**", "/uploads/**", "/login",
-						"/signup", "/logout", "/error", "/error/**", "/memory/**", "memories/**", "/about", "/try", "/api/memory/**",
+						"/signup", "/logout", "/error", "/error/**", "/memory/**", "/memories/**", "/about", "/try", "/api/memory/**",
 						"/upload-error", "/forgot-password/**", "/legal-notices", "/privacy-policy", "/conditions", "/profil",
 						"/profil/activation/**", "/sitemap.xml", "/sracn/**")
 				.permitAll().requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated())
